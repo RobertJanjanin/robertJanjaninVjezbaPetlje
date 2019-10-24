32896f54cf6855ce67175349f9af3e8d5bf4c8a9
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2019-10-24 11:30:32 +0200

    Add files via upload

---
+++ janjaninRobertVjezbaPetlje/janjaninRobertVjezbaPetlje/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robertJanjaninPetlje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unosTeksta;
+            Console.WriteLine("Unesi riječ: ");
+            unosTeksta = Console.ReadLine();
+
+            Console.WriteLine(unosTeksta);
+
+            for (int i = unosTeksta.Length; i > 0; i--)
+            {
+                Console.WriteLine(unosTeksta.Substring(0, i - 1));
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
